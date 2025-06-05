73076a3b99e278c2597d34237d470ceef2cba75c
SinanZ560 <sinanzhu99@gmail.com>
2025-06-05 09:25:06 +0800

    Add files via upload

---
+++ 1_pi_est.R
@@ -0,0 +1,3 @@
+pi_estimation_result <- pi_estimation(expression_matrix = GTEx_epithelial_genes,
+                                      prior = GTEx_prior,
+                                      n_iteration = 5)
